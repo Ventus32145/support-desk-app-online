11387d31713d0a5bcc268a33228f80da232b6623
Rasiel186 <73579466+Rasiel186@users.noreply.github.com>
2024-10-26 12:18:14 +0800

    first commit

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatBox from "../components/ChatBox";
+
+export default function HomePage() {
+    return <div> 
+        <h1>HomePage</h1> 
+        <p>Website Contents</p>
+        <ChatBox/>  
+    </div>;
+}
